88c20655a9ec95ab6516b2edbc5b94a4cf4a299c
tomaspizarrorf <tomipizarro6@gmail.com>
2020-02-21 11:30:57 -0300

    Trabajos en LowComponent

--- src/Componentes/LowComponent/LowComponent.js
+++ src/Componentes/LowComponent/LowComponent.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './LowComponent.css'
 
 
-
 class LowComponent extends Component {
 
     state = {
@@ -12,31 +11,66 @@ class LowComponent extends Component {
             { title: "title1", info: 'info1' },
             { title: "title2", info: 'info2' },
             { title: "title3", info: 'info3' },
+
         ],
+
+        showTarjetas: 0,
     }
+
+
+    sumarTarjetasHandler = () => {
+
+        if (this.state.showTarjetas <= 3) {
+
+            this.setState({ showTarjetas: this.state.showTarjetas + 1 })
+
+        }
+
+    }
+
+    restarTarjetasHandler = () => {
+
+        if (this.state.showTarjetas <= 3) {
+
+            this.setState({ showTarjetas: this.state.showTarjetas - 1 })
+
+        }
+
+    }
+
+
     render() {
+
+        if (this.state.showTarjetas === 1) {
+
+            alert('se mostrara el primer componente')
+
+
+        } else if (this.state.showTarjetas === 2) {
+
+            alert('se mostrara el segundo componente')
+
+        } else if (this.state.showTarjetas === 3) {
+            alert('se mostrara el tercer componente')
+        }
         return (
+
+
             <div className="main_low_component">
                 <ul>
-                    <li href="/">
-                        <h1>Title</h1>
-                        <p>Content Content Content Content Content Content</p>
-                    </li>
-                    <li href="/">
-                        <h1>Title</h1>
-                        <p>Content Content Content Content Content Content</p>
-                    </li>
-                    <li href="/">
-                        <h1>Title</h1>
-                        <p>Content Content Content Content Content Content
-                            
-                        </p>
-                    </li>
-                    <li href="/">
-                        <h1>Title</h1>
-                        <p>Content Content Content Content Content Content</p>
-                    </li>
-                    
+                    <div className="main_low_arrow_left">
+                        <a className="arrow left">a</a>
+                    </div>
+                    <div className="main_low_tarjetas">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                    <div className="main_low_arrow_right">
+                        <i className="arrow right"></i>
+                    </div>
                 </ul>
             </div>
         );
